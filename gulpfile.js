fbe7afdcb0f209bab627e3744416d29c5e9c2c7c
Jim <jim@advancebass.com>
2020-01-06 16:16:22 -0800

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,96 @@
-const gulp = require("gulp");
-const terser = require("gulp-terser");
-const rename = require("gulp-rename");
-const browserSync = require("browser-sync");
-const cssnano = require("gulp-cssnano");
-const autoprefixer = require("gulp-autoprefixer");
-const eslint = require("gulp-eslint");
+const gulp = require('gulp');
+const terser = require('gulp-terser');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
+const cssnano = require('gulp-cssnano');
+const autoprefixer = require('gulp-autoprefixer');
+const eslint = require('gulp-eslint');
+const sass = require('gulp-sass');
 // const pug = require("gulp-pug");
 // const webpack = require("webpack-stream");
 
-gulp.task("watch", function() {
-  gulp.watch("js/*.js", gulp.series("scripts"));
-  gulp.watch("css/*.css", gulp.series("styles"));
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', gulp.series('scripts'));
+  gulp.watch('css/*.css', gulp.series('styles'));
   // gulp.watch("js/*.js", gulp.series("webpack"));
   // gulp.watch("*.pug", gulp.series("pug")).on("change", browserSync.reload);
 });
 
-gulp.task("lint", function() {
+gulp.task('lint', function() {
   return gulp
-    .src("./js/*.js")
+    .src('./js/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
 gulp.task(
-  "scripts",
-  gulp.series("lint", function() {
+  'scripts',
+  gulp.series('lint', function() {
     return gulp
-      .src("./js/*.js")
+      .src('./js/*.js')
       .pipe(terser())
-      .pipe(rename({ extname: ".min.js" }))
-      .pipe(gulp.dest("./build/js"));
+      .pipe(rename({ extname: '.min.js' }))
+      .pipe(gulp.dest('./build/js'));
   })
 );
 
-gulp.task("styles", function() {
+gulp.task('styles', function() {
   return gulp
-    .src("./css/*.css")
+    .src('./css/*.css')
     .pipe(
       autoprefixer({
-        browsers: ["last 2 versions"]
+        browsers: ['last 2 versions']
       })
     )
     .pipe(cssnano())
-    .pipe(rename({ extname: ".min.css" }))
-    .pipe(gulp.dest("./build/css"));
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task("browser-sync", function() {
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/style.scss')
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: './'
     }
   });
 
   gulp
-    .watch(["*.html", "build/js/bundle.js", "build/css/*.css", "build/js/*.js"])
-    .on("change", browserSync.reload);
+    .watch(['*.html', 'build/js/bundle.js', 'build/css/*.css', 'build/js/*.js'])
+    .on('change', browserSync.reload);
 });
 
-gulp.task("default", gulp.parallel("browser-sync", "watch"));
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
 
 // webpack option to allow code-splitting e.g. using import
 gulp.task(
-  "webpack",
-  gulp.series("lint", function() {
+  'webpack',
+  gulp.series('lint', function() {
     return gulp
-      .src("./js/*.js")
+      .src('./js/*.js')
       .pipe(
         webpack({
           output: {
-            filename: "bundle.js"
+            filename: 'bundle.js'
           }
         })
       )
-      .pipe(gulp.dest("./build/js"));
+      .pipe(gulp.dest('./build/js'));
   })
 );
 
